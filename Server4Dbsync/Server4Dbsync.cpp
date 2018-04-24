aa19f99fdb7d6d78bc1daada6f1809d7d4d7abac
nehalem518 <nehalem518@users.noreply.github.com>
2018-04-24 18:36:18 +0800

    server2

--- Server4Dbsync/Server4Dbsync.cpp
+++ Server4Dbsync/Server4Dbsync.cpp
@@ -1,53 +1,120 @@
-//服务器端代码：  
-#include <WINSOCK2.H>  
-#include <stdio.h>  
-#pragma comment(lib,"ws2_32.lib")  
-int main()
+#include <stdio.h>    
+#include <winsock2.h> // 必须包含windwos.h之前  
+#include <Windows.h>  
+
+#pragma comment(lib,"ws2_32.lib")   
+
+#define  PORT 6000  
+DWORD WINAPI clientProc(LPARAM lparam)
 {
-	//创建套接字  
-	WORD myVersionRequest;
-	WSADATA wsaData;
-	myVersionRequest = MAKEWORD(1, 1);
-	int err;
-	err = WSAStartup(myVersionRequest, &wsaData);
-	if (!err)
+	SOCKET sockClient = (SOCKET)lparam;
+	char buf[1024];
+	while (TRUE)
 	{
-		printf("已打开套接字\n");
-
+		memset(buf, 0, sizeof(buf));
+		// 接收客户端的一条数据   
+		int ret = recv(sockClient, buf, sizeof(buf), 0);
+		//检查是否接收失败  
+		if (SOCKET_ERROR == ret)
+		{
+			printf("socket recv failed\n");
+			closesocket(sockClient);
+			return -1;
+		}
+		// 0 代表客户端主动断开连接  
+		if (ret == 0)
+		{
+			printf("client close connection\n");
+			closesocket(sockClient);
+			return -1;
+		}
+		// 发送数据  
+		ret = send(sockClient, buf, strlen(buf), 0);
+		//检查是否发送失败  
+		if (SOCKET_ERROR == ret)
+		{
+			printf("socket send failed\n");
+			closesocket(sockClient);
+			return -1;
+		}
 	}
-	else
+	closesocket(sockClient);
+	return 0;
+}
+bool InitNetEnv()
+{
+	// 进行网络环境的初始化操作  
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 	{
-		//进一步绑定套接字  
-		printf("嵌套字未打开!");
-		return 0;
+		printf("WSAStartup failed\n");
+		return false;
 	}
-	SOCKET serSocket = socket(AF_INET, SOCK_STREAM, 0);//创建了可识别套接字  
-	//需要绑定的参数  
-	SOCKADDR_IN addr;
-	addr.sin_family = AF_INET;
-	addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//ip地址  
-	addr.sin_port = htons(6000);//绑定端口  
-
-	bind(serSocket, (SOCKADDR*)&addr, sizeof(SOCKADDR));//绑定完成  
-	listen(serSocket, 5);//其中第二个参数代表能够接收的最多的连接数  
-
-	//////////////////////////////////////////////////////////////////////////  
-	//开始进行监听  
-	//////////////////////////////////////////////////////////////////////////  
-	SOCKADDR_IN clientsocket;
-	int len = sizeof(SOCKADDR);
-	while (1)
+	return true;
+}
+int main(int argc, char * argv[])
+{
+	if (!InitNetEnv())
 	{
-		SOCKET serConn = accept(serSocket, (SOCKADDR*)&clientsocket, &len);//如果这里不是accept而是conection的话。。就会不断的监听  
-		char sendBuf[100];
-
-		sprintf(sendBuf, "welcome %s to bejing", inet_ntoa(clientsocket.sin_addr));//找对对应的IP并且将这行字打印到那里  
-		send(serConn, sendBuf, strlen(sendBuf) + 1, 0);
-		char receiveBuf[100];//接收  
-		recv(serConn, receiveBuf, strlen(receiveBuf) + 1, 0);
-		printf("%s\n", receiveBuf);
-		closesocket(serConn);//关闭  
-		WSACleanup();//释放资源的操作  
+		return -1;
 	}
+	// 初始化完成，创建一个TCP的socket  
+	SOCKET sServer = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	//检查是否创建失败  
+	if (sServer == INVALID_SOCKET)
+	{
+		printf("socket failed\n");
+		return -1;
+	}
+	printf("Create socket OK\n");
+	//进行绑定操作  
+	SOCKADDR_IN addrServ;
+	addrServ.sin_family = AF_INET; // 协议簇为IPV4的  
+	addrServ.sin_port = htons(PORT); // 端口  因为本机是小端模式，网络是大端模式，调用htons把本机字节序转为网络字节序  
+	addrServ.sin_addr.S_un.S_addr = INADDR_ANY; // ip地址，INADDR_ANY表示绑定电脑上所有网卡IP  
+	//完成绑定操作  
+	int ret = bind(sServer, (sockaddr *)&addrServ, sizeof(sockaddr));
+	//检查绑定是否成功  
+	if (SOCKET_ERROR == ret)
+	{
+		printf("socket bind failed\n");
+		WSACleanup(); // 释放网络环境  
+		closesocket(sServer); // 关闭网络连接  
+		return -1;
+	}
+	printf("socket bind OK\n");
+	// 绑定成功，进行监听  
+	ret = listen(sServer, 10);
+	//检查是否监听成功  
+	if (SOCKET_ERROR == ret)
+	{
+		printf("socket listen failed\n");
+		WSACleanup();
+		closesocket(sServer);
+		return -1;
+	}
+	printf("socket listen OK\n");
+	// 监听成功  
+	sockaddr_in addrClient; // 用于保存客户端的网络节点的信息  
+	int addrClientLen = sizeof(sockaddr_in);
+	while (TRUE)
+	{
+		//新建一个socket，用于客户端  
+		SOCKET *sClient = new SOCKET;
+		//等待客户端的连接  
+		*sClient = accept(sServer, (sockaddr*)&addrClient, &addrClientLen);
+		if (INVALID_SOCKET == *sClient)
+		{
+			printf("socket accept failed\n");
+			WSACleanup();
+			closesocket(sServer);
+			delete sClient;
+			return -1;
+		}
+		//创建线程为客户端做数据收发  
+		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)clientProc, (LPVOID)*sClient, 0, 0);
+	}
+	closesocket(sServer);
+	WSACleanup();
 	return 0;
 }
